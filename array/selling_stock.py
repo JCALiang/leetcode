109529841eeba785f517f8e510c341894c2430e8
joan.liang <joan.liang@accenture.com>
2021-01-04 16:02:15 +1100

    selling stock

---
+++ array/selling_stock.py
@@ -0,0 +1,28 @@
+
+
+def maxProfit( prices):
+    max_profit = 0
+    if not prices:
+        return max_profit
+    min_cost = prices[0]
+
+    for i in prices:
+        if i < min_cost:
+            min_cost = i
+        elif (i-min_cost) > max_profit:
+            max_profit = i-min_cost
+
+    return max_profit
+
+
+
+if __name__ == "__main__":
+    prices=[7,1,5,3,6,4]
+    print(maxProfit(prices))
+
+
+''' trick is to track 2 variables:
+    min_cost
+    and 
+    MAX_PROFIT
+'''
